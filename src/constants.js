6f612b79e34661549f1e40b26e1efd7611f8bc3f
archit gupta <architgupta300@gmail.com>
2025-02-11 00:59:51 +0530

    Unignored constants.js

---
+++ src/constants.js
@@ -0,0 +1,54 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Archit';
+
+export const experiences = [
+    {
+        'company': 'CDN Technologies',
+        'role': 'Web Developer',
+        'duration': 'August 2024 - October 2024',
+        'logo': 'https://5.imimg.com/data5/SELLER/Default/2021/2/IX/US/TO/3553780/cdn-logo2-500x500.jpg',
+        'points': [
+            'Involved in dynamic contributions to web📱 development projects, actively influencing and improving project outcomes.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+    },
+    {
+        'company': 'Codsoft',
+        'role': 'Web Developer Intern',
+        'duration': 'November 2023 - December 2023',
+        'logo': "https://media.licdn.com/dms/image/D5612AQHjetZdoPFD0w/article-cover_image-shrink_720_1280/0/1719745041771?e=2147483647&v=beta&t=lT6nKGhhYsdQearVXlHfbgnX9QXos8adPa_XoDYvWxU",
+        'points': [
+            'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
+            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.'
+        ],
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
+
+console.log("EMAIL_JS_PUBLIC_KEY:", process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY);
+
+
+
+
